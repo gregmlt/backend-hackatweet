f129bdad6b832e2d4b4c209e5818323b60830fb3
GregoireMeillet <meillet.gregoire@gmail.com>
2024-05-02 11:35:38 +0200

    schema/models

--- models/tweets.js
+++ models/tweets.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 
 const tweetSchema = mongoose.Schema({
-  text: String,
-  password: String,
+  content: {
+    type: String,
+    maxlength: 280, // Limiter à 280 caractères
+  },
   like: [
     {
       type: mongoose.Schema.Types.ObjectId,
